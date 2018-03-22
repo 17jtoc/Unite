51de206e5313ec5f57d53ecf42147876d0130ec9
17jtoc <17jtoc@queensu.ca>
2018-03-22 14:00:58 -0400

    Buttons work now

---
+++ app/src/main/java/com/example/jtoco/unite/CategorySelection.java
@@ -0,0 +1,13 @@
+package com.example.jtoco.unite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategorySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.category_selection);
+    }
+}
--- app/src/main/java/com/example/jtoco/unite/IntroFollow.java
+++ app/src/main/java/com/example/jtoco/unite/IntroFollow.java
@@ -1,13 +1,28 @@
 package com.example.jtoco.unite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class IntroFollow extends AppCompatActivity {
+
+
+    private ImageButton b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_follow);
+
+        b = (ImageButton)findViewById(R.id.followbutton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent m = new Intent(IntroFollow.this,CategorySelection.class);
+                startActivity(m);
+            }
+        });
     }
 }
