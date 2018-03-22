6dc860f31b427ce8fa4dae6b3ab33bafd44535bb
17jtoc <17jtoc@queensu.ca>
2018-03-21 23:07:13 -0400

    Buttons work now

--- app/src/main/java/com/example/jtoco/unite/IntroScreen.java
+++ app/src/main/java/com/example/jtoco/unite/IntroScreen.java
@@ -1,13 +1,28 @@
 package com.example.jtoco.unite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class IntroScreen extends AppCompatActivity {
+
+    private ImageButton b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_screen);
+
+        b = (ImageButton)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent z = new Intent(IntroScreen.this,IntroFollow.class);
+                startActivity(z);
+            }
+        });
     }
 }
